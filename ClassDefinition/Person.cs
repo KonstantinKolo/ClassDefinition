a7bb93034257ea03a26585a602099f94b8f76481
Konstantin <konstantinkolovski_zh21@schoolmath.eu>
2023-04-23 14:52:20 +0300

    Initial commit

---
+++ ClassDefinition/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDefinition
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("This is {0} and he is {1}", name, age);
+        }
+    }
+}
---
+++ ClassDefinition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDefinition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.Name = "Konstantin";
+            personOne.Age = 16;
+
+            Person personTwo = new Person();
+            personTwo.Name = "Vasko";
+            personTwo.Age = 15;
+
+            Person personThree = new Person();
+            personThree.Name = "Aleks";
+            personThree.Age = 19;
+
+            personOne.IntroduceYourself();
+            personTwo.IntroduceYourself();
+            personThree.IntroduceYourself();
+        }
+    }
+}
